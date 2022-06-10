e3ae1735836728987b231c7b4ab91237636b533e
= <=>
2022-06-10 15:32:54 +0200

    Add simple function to read config files

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -10,6 +10,16 @@ function Get-GitPull { git.exe pull }
 function Get-GitPush { git.exe push }
 function Set-HomePS { Set-Location "~\OneDrive - Emma Sleep GmbH\Dokumente\WindowsPowerShell\"}
 
+function Import-Config { 
+   [CmdletBinding()]
+   param (
+       [Parameter(Mandatory=$True,Position=1)]
+       [String]
+       $File
+   )
+   return Get-Content $File -ErrorAction Stop | Out-String | ConvertFrom-Json 
+}
+
 # Define Aliases
 New-Alias -Name gstatus -Value Get-GitStatus
 New-Alias -Name gpull -Value Get-GitPull
@@ -17,9 +27,15 @@ New-Alias -Name gpush  -Value Get-GitPush
 New-Alias -Name cdps -Value Set-HomePS
 
 Write-Host "    Set some known Path"
-$me = @{
+$PS5 = @{
    scripts = "~\OneDrive - Emma Sleep GmbH\Dokumente\WindowsPowerShell\Scripts\"
    modules = "~\OneDrive - Emma Sleep GmbH\Dokumente\WindowsPowerShell\Modules\"
+   devs = "~\devs\"
+   gitprovisioning = "~\devs\github-provisioning\etc\iac\provisioning\resources\repositories-definitions\it-repositories"
+}
+$PS = @{
+   scripts = "~\OneDrive - Emma Sleep GmbH\Dokumente\PowerShell\Scripts\"
+   modules = "~\OneDrive - Emma Sleep GmbH\Dokumente\PowerShell\Modules\"
    devs = "~\devs\"
    gitprovisioning = "~\devs\github-provisioning\etc\iac\provisioning\resources\repositories-definitions\it-repositories"
 }
