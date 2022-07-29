2a7332d0eb29f9ece1411ae3ce0ebe5668efcf26
= <=>
2022-07-29 18:20:48 +0200

    Fix Certificate problem by deleting and reimporting it

---
+++ Import-AutobotsCertificate.ps1
@@ -0,0 +1,21 @@
+Import-Module psmodule-credentials
+Get-LoggedUser -Reload
+
+# Removing the Certificate
+Get-ChildItem Cert:\LocalMachine\My\ada909043ea4385a15e5d5f812372b1b6efb8139 | Remove-Item
+
+# Get Certificate password
+$certpwd = ConvertTo-SecureString -String (Set-CredentialFromFile -Type Autobotscertificate) -AsPlainText -Force
+
+# Import the Certificate
+Import-PFXCertificate -FilePath C:\Users\nicolaskapfer\.credentials\EmmaIT-Autobots.pfx -CertStoreLocation Cert:\LocalMachine\My -Password $certpwd
+
+
+#$certpassword = Set-CredentialFromFile -Type Autobotscertificate | ConvertTo-SecureString
+
+#if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { 
+#    #$certpassword = Get-Credential -UserName "Autobots Certificate"
+#    Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -Command `"Import-Module psmodule-credentials; Get-LoggedUser -Reload; $certpwd = ConvertTo-SecureString -String (Set-CredentialFromFile -Type Autobotscertificate) -AsPlainText -Force ; Import-PFXCertificate -FilePath C:\Users\nicolaskapfer\.credentials\EmmaIT-Autobots.pfx -CertStoreLocation Cert:\LocalMachine\My -Password $certpwd; Start-Sleep 10`"" -Verb RunAs
+#    #Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -Command `"Write-Host $($certpassword.Password); Start-Sleep 2`"" -Verb RunAs
+#    exit
+#}
--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -10,16 +10,6 @@ function Get-GitPull { git.exe pull }
 function Get-GitPush { git.exe push }
 function Set-HomePS { Set-Location "~\OneDrive - Emma Sleep GmbH\Dokumente\WindowsPowerShell\"}
 
-function Import-Config { 
-   [CmdletBinding()]
-   param (
-       [Parameter(Mandatory=$True,Position=1)]
-       [String]
-       $File
-   )
-   return Get-Content $File -ErrorAction Stop | Out-String | ConvertFrom-Json 
-}
-
 # Define Aliases
 New-Alias -Name gstatus -Value Get-GitStatus
 New-Alias -Name gpull -Value Get-GitPull
@@ -49,8 +39,16 @@ oh-my-posh --init --shell pwsh --config ~\.psprofile\nicolaskapfer.omp.json | In
 # Fix for Connect-ExchangeOnline
 # (https://cloudinfra.net/how-to-fix-error-new-exopssession-create-powershell-session-is-failed-using-oauth/) 
 if((Get-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WinRM\Client" -Name "AllowBasic").AllowBasic -eq 0){
+   Write-Host "    Fixing OAuth for Exchange Online connection"
    if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { 
       Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -Command `"Set-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\WinRM\Client -Name AllowBasic -Value 1`"" -Verb RunAs
-      exit 
+      exit
    }
+}
+
+# Fix for problematic Certificate (Autobots)
+Write-Host "    Import Autobots certificate"
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { 
+   Start-Process pwsh.exe "-NoProfile -ExecutionPolicy Bypass -File `".psprofile\Import-AutobotsCertificate.ps1`"" -Verb RunAs
+   exit
 }
