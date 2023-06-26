166bef56139c9aef357a0d779cd1b57fefdbb994
= <=>
2023-06-26 13:50:51 +0200

    Config + FW Rule (RDP Home Office) for Animatronio

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,12 @@
-Write-Host "    Running personal Profile"
+Write-Host "    Running personal Profile" -ForegroundColor Cyan
 
 # Import Modules
 #Import-Module oh-my-posh -UseWindowsPowerShell
 Import-Module KNSModulesDocsGenerator -Force 3>$null
+
+Write-Host "    Import personal configuration" -ForegroundColor DarkCyan
+# Import Config
+$myconfig = Import-Config "~\.psprofile\config.json"
 
 # Functions Definition for Aliases
 function Set-HomePS { Set-Location "~\OneDrive - Emma Sleep GmbH\Dokumente\PowerShell\"}
@@ -50,10 +54,40 @@ oh-my-posh --init --shell pwsh --config ~\.psprofile\nicolaskapfer.omp.json | In
 #}
 
 # Fix for problematic Certificate (Autobots)
-Write-Host "    Import Autobots certificate" -ForegroundColor DarkCyan
-if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { 
-   Start-Process pwsh.exe "-NoProfile -ExecutionPolicy Bypass -File `".psprofile\Import-AutobotsCertificate.ps1`"" -Verb RunAs
-   exit
-}
+#Write-Host "    Import Autobots certificate" -ForegroundColor DarkCyan
+#if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { 
+#   Start-Process pwsh.exe "-NoProfile -ExecutionPolicy Bypass -File `".psprofile\Import-AutobotsCertificate.ps1`"" -Verb RunAs
+#   exit
+#}
 
+Write-Host "    Connecting to Microsoft Graph..." -ForegroundColor DarkCyan
 Connect-MgGraph -Scopes $MgProperties.Scope
+
+Write-Host "    Connecting to Azure..." -ForegroundColor DarkCyan 
+Connect-AzAccount -Subscription $myconfig.Azure.Subscriptions.EmmaIT -AccountId $myconfig.Azure.Account | Out-Null
+
+# Change IP Access for Home Office (on Animatronio)
+$homeofficeIP = (Resolve-DnsName $myconfig.HomeOffice.Hostname).IPAddress
+Write-Host "    Home Office IP: $($homeofficeIP)" -ForegroundColor Blue
+$nsg = Get-AzNetworkSecurityGroup -Name $myconfig.Azure.VMs.Animatronio.NSG -ResourceGroupName $myconfig.Azure.Vms.Animatronio.RG
+$fwr = $nsg | Get-AzNetworkSecurityRuleConfig -Name $myconfig.Azure.VMs.Animatronio.Rule
+if($fwr.SourceAddressPrefix[0] -ne $homeofficeIP){
+   Write-Host "    Updating Firewall Rules for Animatronio" -ForegroundColor DarkRed
+   $params = @{
+      Name                       = $myconfig.Azure.VMs.Animatronio.Rule
+      SourceAddressPrefix        = $homeofficeIP
+      NetworkSecurityGroup       = $nsg
+      Description                = "Allow IP Address from Home Office for $($env:USERNAME) (Updated: $(Get-Date -Format "dd/MM/yyyy"))"
+      Access                     = $fwr.Access
+      Protocol                   = $fwr.Protocol
+      Direction                  = $fwr.Direction
+      Priority                   = $fwr.Priority
+      DestinationAddressPrefix   = $fwr.DestinationAddressPrefix
+      DestinationPortRange       = $fwr.DestinationPortRange
+      SourcePortRange            = $fwr.SourcePortRange
+
+   }
+
+   Set-AzNetworkSecurityRuleConfig @params
+   Set-AzNetworkSecurityGroup -NetworkSecurityGroup $nsg
+}
--- README.md
+++ README.md
@@ -16,5 +16,5 @@ git clone git@github.com:nicolaskapfer/psprofile.git ~/.psprofile
 Then link the Profile to "Current User / Current Host" aka `$profile`
 
 ```powershell
-New-Item -Path $profile -Target ~\.psprofile\Microsoft.PowerShell_profile.ps1
+New-Item -Path $profile -Target ~\.psprofile\Microsoft.PowerShell_profile.ps1 -Type SymbolicLink
 ```
